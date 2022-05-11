7e7db708c827a9d4e7b07c0c6e0eae3e8e636827
Sydney Sanders <84546691+slimmsyd@users.noreply.github.com>
2022-05-11 16:30:55 -0400

    Add files via upload

--- Contracts/ProposalIdBackendAndFrontend/index.js
+++ Contracts/ProposalIdBackendAndFrontend/index.js
@@ -1,177 +1,586 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {ethers,  providers, Contract, BigNumber} from 'ethers';
-import {useState, useEffect, useRef} from "react";
-import {web3} from 'web3';
-import Web3modal from 'web3modal';
-import {FETCHIDADDRESS, FETCHVOTESADDRESS, fetchidabi, fetchvotesabi} from '../constants'
-import Hero from '../components/Hero';
-
-export default function Home() {
-  const zero = BigNumber.from(0);
-  const [isConnected, setIsConnected] = useState(false);
-  const [accountAddress, setAccountAddress] = useState('');
-  const [loading, setLoading] = useState(false);
-  //Dealing with LINK amount 
-  const [linkTokens, setLinkTokens] = useState(zero); 
-  const [etherBalance, setEtherBalance] = useState(zero);
-  //PropsalID
-  const[proposalId, setProposalId] = useState("")
+export const FETCHIDADDRESS = "0xd9471f833C88a709c026cae6882eB70c70e90951"
+export const FETCHVOTESADDRESS = "0xc6802E4DBa52063a642Ca086a7258a23d4DC160C"
 
 
-
-  const web3modalref = useRef(); 
-  useEffect(() => { 
-    web3modalref.current = new Web3modal( { 
-        network: "kovan",
-        providerOptions: {},
-        disableInjectedProvider: false,
-    })
-    Connect();
-  });
-
-  const splitString = (string) => {
-    let result1 = string.substring(0,5);
-    let result2 = string.substring(38,string.length);
-    let finalResult = result1 + "..." + result2;
-    return finalResult;
-  };
-  
-  //get provider or signer 
-  const getProviderOrSigner = async(needSigner = false) => { 
-    const provider = await web3modalref.current.connect(); 
-    const web3provider = new providers.Web3Provider(provider);
-  
-    const signer = web3provider.getSigner(); 
-    const address = await signer.getAddress();
-    const substringAddress = splitString(address);
-    setAccountAddress(substringAddress);
-    console.log(substringAddress)
-
-    //check if chainID is kovan 
-    const {chainId} = await web3provider.getNetwork(); 
-    if(chainId !== 42) { 
-      window.alert("You are on the Wrong Network, Swich to Kovan"); 
-    }
-
-    if(needSigner) { 
-      const signer = web3provider.getSigner();
-      return signer;
-    }
-    return web3provider;
+export const fetchidabi =   [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_linkToken",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "bytes32",
+        "name": "id",
+        "type": "bytes32"
+      }
+    ],
+    "name": "ChainlinkCancelled",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "bytes32",
+        "name": "id",
+        "type": "bytes32"
+      }
+    ],
+    "name": "ChainlinkFulfilled",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "bytes32",
+        "name": "id",
+        "type": "bytes32"
+      }
+    ],
+    "name": "ChainlinkRequested",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "bytes32",
+        "name": "requestId",
+        "type": "bytes32"
+      },
+      {
+        "indexed": true,
+        "internalType": "bytes",
+        "name": "data",
+        "type": "bytes"
+      }
+    ],
+    "name": "RequestFulfilled",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "_destAddr",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "TransferSent",
+    "type": "event"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "fallback"
+  },
+  {
+    "inputs": [],
+    "name": "LinkToken",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "Proposals",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "propId",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "counter",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "data",
+    "outputs": [
+      {
+        "internalType": "bytes",
+        "name": "",
+        "type": "bytes"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "requestId",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes",
+        "name": "bytesData",
+        "type": "bytes"
+      }
+    ],
+    "name": "fulfillBytes",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes",
+        "name": "buffer",
+        "type": "bytes"
+      }
+    ],
+    "name": "iToHex",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "proposalId",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "requestBytes",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "returnLinkBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "returnProposalId",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalAmountOfLink",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "contract IERC20",
+        "name": "token",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferERC20",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "receive"
   }
-  //connection handler 
-  const Connect = async() => { 
-    try { 
-      await getProviderOrSigner();
-      setIsConnected(true);
-    }catch(err) { 
-      console.error(err);
+]
+
+
+  export const fetchvotesabi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address payable",
+          "name": "_address",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "id",
+          "type": "bytes32"
+        }
+      ],
+      "name": "ChainlinkCancelled",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "id",
+          "type": "bytes32"
+        }
+      ],
+      "name": "ChainlinkFulfilled",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "id",
+          "type": "bytes32"
+        }
+      ],
+      "name": "ChainlinkRequested",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferRequested",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "bytes32",
+          "name": "requestId",
+          "type": "bytes32"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "yays",
+          "type": "uint256"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "nays",
+          "type": "uint256"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "abstensions",
+          "type": "uint256"
+        }
+      ],
+      "name": "RequestFulfilledVotes",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "acceptOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes32",
+          "name": "requestId",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_yays",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_nays",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_abstensions",
+          "type": "uint256"
+        }
+      ],
+      "name": "fulfillVotes",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "lastRequestIdReceived",
+      "outputs": [
+        {
+          "internalType": "bytes32",
+          "name": "",
+          "type": "bytes32"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "lastRequestIdSent",
+      "outputs": [
+        {
+          "internalType": "bytes32",
+          "name": "",
+          "type": "bytes32"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "noAbstenstions",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "noNays",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "noYays",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "proposalIdURL",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_id",
+          "type": "uint256"
+        }
+      ],
+      "name": "requestVotes",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
     }
-  }
-
-  //Create a function to fund the contract
-  const getLinkBalance = async() => { 
-    try { 
-      const provider = await getProviderOrSigner(); 
-      const contract = new Contract(
-        FETCHIDADDRESS,
-        fetchidabi,
-        provider
-      );
-      const balance = await contract.returnLinkBalance();
-      balance = ethers.utils.formatEther(balance);
-      setLinkTokens(balance);
-      console.log(balance)
-    }catch(err) { 
-      console.error(err);
-    };
-  }
-  //function returns balance of ETH in contract
-  const returnETH = async() => { 
-    try { 
-      const provider = await getProviderOrSigner()
-      const contract= new Contract(
-        FETCHIDADDRESS,
-        fetchidabi,
-        provider
-      );
-      const balance = await contract.getBalance();
-      balance = ethers.utils.formatEther(balance);
-      setEtherBalance(balance);
-      console.log(balance)
-    }catch(err) { 
-      console.error(err);
-    }
-  }
-  //Depost or Send link to contract
-  const depositLink = async(tokenAddress, to, from) =>  { 
-    try {
-      const signer = await getProviderOrSigner(true); 
-      const contract = new Contract( 
-        FETCHIDADDRESS,
-        fetchidabi,
-        signer
-      );
-      //Place in parameters to send Link
-      const tx = await contract.transfer(tokenAddress, to, from);
-      setLoading(true)
-      tx.wait();
-      setLoading(false);
-    }catch(err) { 
-      console.error(err);
-    }
-  };
-
-  const requestBytes = async() => { 
-    try { 
-      const signer = await getProviderOrSigner(true); 
-      const contract = new Contract(
-        FETCHIDADDRESS,
-        fetchidabi,
-        signer
-      );
-      const tx = await contract.requestBytes();
-      setLoading(true);
-      //wait for transaction to get mined
-      await tx.wait();
-      setLoading(false);
-
-    }catch(err) { 
-      console.error(err);
-    }
-
-  }
-
-
-
-
-
-
-
-
-
-  return (
-   <>
-    <Head>
-      <title>Metal Rentals</title>
-      <meta name="description" content="Bridging community and Travel. Find vaction homes, apartments cabins on MetaRentals " />
-        <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <main>
-      <Hero />
-      <button onClick={returnETH}>Get ETH Balance</button>
-      <button onClick={getLinkBalance}>Get Link Balance</button>
-      <button onClick={requestBytes}>Request Latest DAO IDs</button>
-
-    </main>
-   
-   
-   </>
-
-
-
-  )
-}
+  ]
